4bd7c10d87fc6cb76c8a48086d5f05c95a805966
Iva Ivanova <ivaivanova091@gmail.com>
2022-10-10 22:06:01 +0100

    Adding coments in assign/main.go file.

--- src/assign/main.go
+++ src/assign/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	// For assignment operators the operands must be of the same type.
+	
 	var a, b int
 
+	// Initialization of multiple variables in a single statement.
+	// Operator = means assign.
 	a, b = 10, 5
 	fmt.Println("Assigned values:\ta= ", a, "\tb = ", b)
 
-	a += b // a = 10 + 5
+	a += b // a = 10 + 5 but also can be used for concatenate strings.
 	fmt.Println("Addion and assign:\ta= ", a)
 
 	a -= b // a = 15 - 5
