31863ca6334ff70e4588f5ad9945c86e17bd3144
Iva Ivanova <ivaivanova091@gmail.com>
2022-10-18 16:15:25 +0100

    Adding switch cases.
    Results:
    Characters match.
    
    Both expressions are true.
    
    Both expressions are true.
    
    Both conditions are false.
    
    Number is Two.
    
    Less than three.
    
    Although the letter is lower case 'a',
    The letter is the first in the alphabet.

--- src/control_flow/main.go
+++ src/control_flow/main.go
@@ -6,6 +6,8 @@ func main() {
 	// Conditional branching - fundamental programming technique that
 	// offers the program two directions in which to proceed, depending on the results of the evaluation.
 
+	/////////////////////// If - else statements ///////////////////////
+
 	// The else keyword and opening { curly bracket must appear on the same line as the } closing curly bracket of the previous block of statements!
 
 	// Single statement condition.
@@ -32,5 +34,48 @@ func main() {
 		fmt.Println("\n2st condition is true.")
 	} else {
 		fmt.Println("\nBoth conditions are false.")
+	}
+
+	/////////////////////// Switch cases ///////////////////////
+	num := 2
+
+	switch num {
+	case 1:
+		fmt.Println("\nNumber is One.")
+	case 2:
+		fmt.Println("\nNumber is Two.")
+	case 3:
+		fmt.Println("\nNumber is Three.")
+	default:
+		fmt.Println("\nNumber is unrecognized.")
+	}
+
+	// A switch with number of march-values are to each execute the same statements,
+	// only the final case statement need include the statements to be executed.
+
+	switch num {
+	case 0:
+	case 1:
+	case 2:
+		fmt.Println("\nLess than three.")
+	case 3:
+		fmt.Println("\nExactly three.")
+	default:
+		fmt.Println("\nGreater than 3 or negative.")
+	}
+
+	// When a match is found, the switch normally ends, but you can add the fallthrough keyword
+	// as a final statement in any case block to continue to the next case statement.
+
+	ch := 'a'
+
+	switch ch {
+	case 'a':
+		fmt.Printf("\nAlthough the letter is lower case 'a',")
+		fallthrough
+	case 'A':
+		fmt.Println("\nThe letter is the first in the alphabet.")
+	default:
+		fmt.Println("\nLetter is unrecognized.")
 	}
 }
