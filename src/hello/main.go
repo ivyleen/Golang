14ea0e7c5c90ea78a77c01cc0a8a23fbba75dc36
Iva Ivanova <ivaivanova091@gmail.com>
2022-10-19 11:58:27 +0100

    Adding more information about packages and import keyword.

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,13 +1,27 @@
 package main
 
+// package = project = workspace
+// Executable package = generates a file that we can run.
+// Reusable package = code used as "helpers". Good place to put reusable logic. Aka code dependecies or libraries.
+// How we know which kind are we making? By the name. For example: main is executable package. Anything else is a reusable.
+// In package main there must be always a function called main.
+
+import "fmt"
+
 /*
 If there are multiple lines for importing:
 import (
 	"fmt"
 	"strings"
-)*/
+)
 
-import "fmt"
+Also import mean: give my package <package name> access to all the code and functionality
+that is contained inside of this other package called <imported package name>.
+
+Standard library can be found on: www.golang.org/pkg
+For example:
+fmt = standartized format package
+*/
 
 func main() {
 	fmt.Println("Hello world!")
